014d35cb91d9c4670e5f137fc1170a79d51eb222
Max <jeserodri7@gmail.com>
2020-04-28 00:18:21 +0300

    add cards and handle click on card

--- script.js
+++ script.js
@@ -33,7 +33,11 @@ class WeatherServiceAPI {
             return res.json()
         })
     }
+
+
 }
+
+
 
 
 class WeatherModel {
@@ -48,7 +52,6 @@ class WeatherModel {
         return this.resp.list.slice(0, stepsCount)
     }
     getCurrentWeatherDesc(){
-        // debugger
         return this.getCurrentWeather().weather[0].description;
     }
 
@@ -68,6 +71,21 @@ class WeatherModel {
         }
     }
 
+    getForecastGroupdByDays(){
+        return this.resp.list.reduce(function (acc , hourlyValue) {
+            let dayOfHourlyValue = new Date (hourlyValue.dt_txt).getDate()
+            let monthOfHourlyValue = new Date (hourlyValue.dt_txt).getMonth()
+
+            let dayKey = dayOfHourlyValue < 10 ? "0" + dayOfHourlyValue : dayOfHourlyValue
+            let key = "" + monthOfHourlyValue + dayKey
+
+            acc[key] || (acc[key] = [])
+            acc[key].push(hourlyValue)
+
+            return acc
+        },{})
+    }
+
     _getCurrentDayDuration(sunset, sunrize){
          let res = moment.utc(
                 moment(new Date(sunset), "HH:mm:ss")
@@ -75,6 +93,8 @@ class WeatherModel {
          ).format("HH:mm")
         return res
     }
+
+
 
 };
 
@@ -104,35 +124,38 @@ function initWeather(resp){
 
 
     let tabsClickHandler = (key) => {
+        weatherContainer.innerHTML = ''
         if (key == TAB_KEYS.TODAY) {
             new TodayView(weatherContainer).render(weather);
         } else if (key == TAB_KEYS.FORECAST){
-            weatherContainer.innerHTML = "privet"
+            new DayForecast(weatherContainer).render(weather);
         }
     }
 
     let tabsContainer = document.createElement("div");
     document.body.prepend(tabsContainer);
+    tabsContainer.style.backgroundColor = "black";
     tabsContainer.style.display = "flex";
+    tabsContainer.style.width = "100%";
+    tabsContainer.style.paddingLeft = "25px";
+    tabsContainer.style.boxSizing = "border-box";
+
+
     new WeatherTabsBlockView(tabsContainer, tabsClickHandler).render(tabsConfig);
 
     tabsClickHandler(TAB_KEYS.TODAY);
-
-
-
 }
 
 if (!navigator.geolocation) {
     WeatherServiceAPI
         .getWeatherByCoords(
             new WeatherByCityDTO(WeatherServiceAPI.NATIVE_CITY)
-        )
-        .then()
+        ).then(initWeather)
 } else {
-    navigator.geolocation.getCurrentPosition((position)=> {
+    navigator.geolocation.getCurrentPosition((position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    // debugger
+
     WeatherServiceAPI
         .getWeatherByCoords(
             new WeatherByCoordsDTO(lat, lon)
@@ -144,9 +167,108 @@ if (!navigator.geolocation) {
             .getWeatherByCoords(
                 new WeatherByCityDTO(WeatherServiceAPI.NATIVE_CITY)
             )
-            .then((res) => { alert('yra blyat')})
+            .then(initWeather)
     });
 }
+
+class DayForecast {
+    constructor(renderToHtmlElem){
+        this.renderToHtmlElem = renderToHtmlElem;
+        this.selectedCardKey = null;
+    }
+
+    render(weather){
+        this.renderToHtmlElem.innerHTML = ''
+
+        let cardContainer = this._renderCards(weather);
+        this.renderToHtmlElem.appendChild(cardContainer);
+
+        let dayForecasts = weather.getForecastGroupdByDays()[this.selectedCardKey];
+        new WeatherEveryThreeHoursCardView(this.renderToHtmlElem)
+            .render(dayForecasts, this._dayHumanize(dayForecasts[0]))
+    }
+
+    _renderCards(weather) {
+        let cardContainer = document.createElement("div");
+        cardContainer.style.display = "flex";
+        cardContainer.style.justifyContent = "space-around";
+
+
+        let forecastsGroupedByDay = weather.getForecastGroupdByDays();
+
+        if (this.selectedCardKey == null) {
+            this._setDefaultselectedCardKey(forecastsGroupedByDay);
+        }
+
+        for (let [key, daysHourlyForecast] of Object.entries(forecastsGroupedByDay)) {
+            let daysTemperatures = daysHourlyForecast.map((hourlyForecast) => {
+                return hourlyForecast.main.temp_max;
+            })
+            let maxDayTemp = Math.max(...daysTemperatures)
+
+            let maxDailyTempState = daysHourlyForecast.find((hourlyForecast) => {
+                return hourlyForecast.main.temp_max == maxDayTemp;
+            })
+
+            let onClickCallback = (key) => {
+                this.selectedCardKey = key;
+                this.render(weather);
+            };
+
+            new DailyTemperatureCardView(cardContainer, onClickCallback)
+                .render(maxDailyTempState, key, this.selectedCardKey);
+
+        }
+        return cardContainer;
+    }
+
+    _setDefaultselectedCardKey(daysGrouped) {
+        this.selectedCardKey = Object.keys(daysGrouped)[0]
+    }
+
+    _dayHumanize(data){
+        return moment(data.dt_txt).format('dddd')
+    }
+}
+
+class DailyTemperatureCardView {
+    constructor(renderToHtmlElem, onClickCallback){
+        this.renderToHtmlElem = renderToHtmlElem;
+        this.onClickCallback = onClickCallback;
+    }
+
+
+    render(dayData, key , selectedCardKey){
+        let cardBody = document.createElement("div");
+
+        cardBody.addEventListener("click", () => {
+            this.onClickCallback(key);
+        });
+
+        if(key == selectedCardKey){
+            cardBody.style.border = '2px solid black'
+        }
+
+        let cardBodyDay = document.createElement("div")
+        cardBodyDay.innerHTML = moment(dayData.dt_txt).format('dddd');
+        cardBody.appendChild(cardBodyDay);
+
+        let cardBodyData = document.createElement("div")
+        cardBodyData.innerHTML = dayData.dt_txt;
+        cardBody.appendChild(cardBodyData);
+
+        let cardBodyTemp = document.createElement("div");
+        cardBodyTemp.innerHTML = dayData.main.temp_max
+        cardBody.appendChild(cardBodyTemp);
+
+        let cardBodyWeather = document.createElement("div");
+        cardBodyWeather.innerHTML = dayData.weather[0].description
+        cardBody.appendChild(cardBodyWeather);
+
+        this.renderToHtmlElem.appendChild(cardBody);
+    }
+}
+
 
 class WeatherTabsBlockView {
     constructor(renderToHtmlElem,tabsClickHandler){
@@ -158,11 +280,20 @@ class WeatherTabsBlockView {
         tabsConfig.forEach((tabConfig) => {
             let headerTab = document.createElement("div");
             this.renderToHtmlElem.appendChild(headerTab);
+
             headerTab.innerHTML = tabConfig.title;
             headerTab.style.paddingRight = "10px";
-            
+            headerTab.style.color = "white";
+            headerTab.style.width = "100px";
+            headerTab.style.padding = "10px ";
+            headerTab.style.textAlign = "center ";
+            headerTab.style.borderLeft = "1px solid white ";
 
+            headerTab.addEventListener("click" , () => {
+                 this.tabsClickHandler(tabConfig.key);
+            })
         })
+
     }
 }
 
@@ -236,7 +367,7 @@ class WeatherEveryThreeHoursCardView {
         this.renderToHtmlElem = renderToHtmlElem;
     }
 
-    render(forecasts) {
+    render(forecasts, currentDayName = 'Today') {
         let header = this._renderHeader();
         this.renderToHtmlElem.appendChild(header);
 
@@ -244,11 +375,9 @@ class WeatherEveryThreeHoursCardView {
         body.style.display = "flex";
         body.style.flexDirection = "column";
 
-
-
         let titles = [
             {
-                title: 'Today',
+                title: currentDayName,
                 data: forecasts.map((forecast) => { return forecast.dt_txt })
             },
             {
